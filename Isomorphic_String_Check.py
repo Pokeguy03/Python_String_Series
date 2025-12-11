a39d159e57affcffec8a2afa24b55366961fcec6
Gurunath_M <142033469+Pokeguy03@users.noreply.github.com>
2025-12-11 20:13:47 +0530

    Create Isomorphic_String_Check.py

---
+++ Isomorphic_String_Check.py
@@ -0,0 +1,15 @@
+t="abb"
+s="xzy"
+m1={}
+m2={}
+for i in range(len(s)):
+    c1=s[i]
+    c2=t[i]
+    if c1 in m1 and m1[c1]!=c2:
+        print(False)
+    if c2 in m2 and m2[c2]!=c1:
+        print(False)
+    m1[c1]=c2
+    m2[c2]=c1
+print(True)
+        
