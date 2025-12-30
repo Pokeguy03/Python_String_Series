f7c4f016bc4c28ea7a3b9f2faa7a3427c682c1c4
Gurunath_M <142033469+Pokeguy03@users.noreply.github.com>
2025-12-30 22:47:02 +0530

    Update First_non_repeating_Character.py

--- First_non_repeating_Character.py
+++ First_non_repeating_Character.py
@@ -8,3 +8,18 @@ for i in s:
         break
 else:
     print("No repeatation")
+
+
+
+Leetcode:
+from collections import Counter
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        li=Counter(s)
+        for i in range(len(s)):
+            if li[s[i]]==1:
+                return i
+                break
+           
+        return -1
+        
