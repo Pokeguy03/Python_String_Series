c319661db34a371d2a82372478467d161b53c6f5
Gurunath_M <142033469+Pokeguy03@users.noreply.github.com>
2025-12-31 16:41:13 +0530

    Update Isomorphic_String_Check.py

--- Isomorphic_String_Check.py
+++ Isomorphic_String_Check.py
@@ -13,3 +13,30 @@ for i in range(len(s)):
     m2[c2]=c1
 print(True)
         
+Leetcode:
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        word=s.split()
+        if len(pattern)!=len(word):
+            return False
+        ctow={}
+        wtoc={}
+        for i in range(0,len(word)):
+            if pattern[i] not in ctow:
+                ctow[pattern[i]]=word[i]
+            elif pattern[i] in ctow:
+                if ctow[pattern[i]]==word[i]:
+                    continue
+                else:
+                    return False
+            if word[i] not in wtoc:
+                wtoc[word[i]]=pattern[i]
+            elif word[i] in wtoc:
+                if wtoc[word[i]]==pattern[i]:
+                    continue
+                else:
+                    return False
+        return True
+
+                    
+        
